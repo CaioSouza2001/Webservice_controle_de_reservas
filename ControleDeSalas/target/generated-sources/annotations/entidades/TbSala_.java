0bd809b5d6792a221ba62f5771e98db9c755a5d7
Caio <caio.souza.lima.2001.08@gmail.com>
2020-02-24 16:32:16 -0300

    Fixando bugs

--- ControleDeSalas/target/generated-sources/annotations/entidades/TbSala_.java
+++ ControleDeSalas/target/generated-sources/annotations/entidades/TbSala_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-23T21:52:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-24T08:16:02")
 @StaticMetamodel(TbSala.class)
 public class TbSala_ { 
 
